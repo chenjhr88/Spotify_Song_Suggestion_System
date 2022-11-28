641b71783abc57bd14d930b2d1ac538a9f19949d
Hayden K <haydenk643@gmail.com>
2022-11-27 21:02:26 -0600

    created temp files to add back on adjlist files

--- code/adjacencylist.h
+++ code/adjacencylist.h
@@ -18,8 +18,6 @@ struct Vertex {
 struct Edge {
     double threshold;
     
-    std::string label;
-    Vertex dest;
     //common characteristic
 };
 
@@ -29,19 +27,7 @@ class Graph {
     public:
         void insertVertices();
 
-        //getting vertex, vertices, and the adjacent vertices of selected vertex
-        Vertex getVertex(int i);
-        std::vector<Vertex> getVertices();
-        std::vector<Vertex> getAdjacents(Vertex v);
-
-        std::string getLabel(Vertex v);
-        void setLabel(Vertex& v, std::string label_str);
-        std::string getLabel(Edge e);
-        void setLabel(Edge& e, std::string label_str);
-
-        std::string getLabel(Vertex v, Vertex w);
-        std::string setLabel(Vertex& v, Vertex& w, std::string label_str);
-
+        
     private:
         std::vector<Vertex> vertices;
         //index represents vertex number
