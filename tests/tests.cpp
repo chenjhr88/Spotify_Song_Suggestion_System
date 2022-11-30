8c02e6f0e273495716dbf7c8e30f84623b0ae8b0
Jason Kandula <kandula7@illinois.edu>
2022-11-30 06:18:23 +0000

    testcases cont.

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,6 +1,7 @@
 #include <catch2/catch_test_macros.hpp>
 
 //set compiler path to fix?
+//lmao nevermind
 #include <iostream>
 #include <fstream>
 #include <ctype.h>
@@ -12,6 +13,16 @@
 
 TEST_CASE("test1", "[valgrind][weight=1]")
 {
+    //{{ "SongName A", "Danceability 90", "Danceability 20"}, \
+    // { "SongName B", "Danceability 20"}, \
+    // { "SongName C", "Danceability 50"}, \
+    // { "SongName D", "Danceability 99", "Danceability 1"}, \
+    // { "SongName E", "Danceability 40", "Danceability 50"}, \
+    // { "SongName F", "Danceability 7"}, \
+    // { "SongName G", "Danceability 34"}, \
+    // { "SongName H", "Danceability 20"}};
+    std::vector<std::vector<string>> test_inputted_vector = 
+    buildAdjacencyMatrix = 
     REQUIRE("HELLO" == "HELLO");
     //test adjacency matrix 1
 }
