1cfc9c91e347409dcc5ecb2f48f2ea16d0e65a22
Jason Kandula <kandula7@illinois.edu>
2022-11-29 22:40:21 -0600

    building test cases 1: just seeing if branch appears

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,10 +1,36 @@
+#include <catch2/catch_test_macros.hpp>
+
+//set compiler path to fix?
 #include <iostream>
 #include <fstream>
 #include <ctype.h>
 #include <time.h>
+#include "adjacencylist.cpp
+#include "bfs_traversal.hpp"
 #include <utility>
+//working on - jason
 
 TEST_CASE("test1", "[valgrind][weight=1]")
 {
     REQUIRE("HELLO" == "HELLO");
+    //test adjacency matrix 1
 }
+
+TEST_CASE("test1", "[valgrind][weight=1]")
+{
+    REQUIRE("HELLO" == "HELLO");
+    //test adjacency matrix 2
+}
+
+TEST_CASE("test1", "[valgrind][weight=1]")
+{
+    REQUIRE("HELLO" == "HELLO");
+    //test adjacency 3
+}
+
+TEST_CASE("test1", "[valgrind][weight=1]")
+{
+    REQUIRE("HELLO" == "HELLO");
+    //traversal
+}
+
