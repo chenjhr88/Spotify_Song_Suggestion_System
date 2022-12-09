a55a44379d93a94f79b8f92e9e740ccf407934d2
Hayden K <haydenk643@gmail.com>
2022-12-09 22:34:42 +0000

    updated adjacecnylist.h to match main

--- src/adjacencylist.h
+++ src/adjacencylist.h
@@ -2,7 +2,10 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 #include <fstream>
+#include <iterator>
+#include <map>
 using namespace std;
 
 
@@ -23,8 +26,8 @@ struct Vertex {
     //this int will help identify what vertex we're looking at
     int vert_num;
     //insert characteristics
-    double dancabililty;
-    double popularity;
+    double dancability;
+    double acousticness;
     double energy;
     std::string song_name;
 
@@ -37,13 +40,11 @@ struct Vertex {
 
 
 class Graph {
-
-
     public:
         Graph();
         std::vector<std::vector<int>> buildAdjacencyMatrix(std::vector<std::vector<std::string>> my_input);
         bool contains(std::vector<std::string> my_vec, std::string item_looking);
-        void insertVertices();
+        void insertVertices(string filename, string filewrite);
         void translateData(const std::string& datainput, const std::string& dataoutput);
 
         void SplitString(const std::string & str1, char sep, std::vector<std::string> &fields);
@@ -53,9 +54,9 @@ class Graph {
         void insertEdge(int v1, int v2);
 
         //getting vertex, vertices, and the adjacent vertices of selected vertex
-        Vertex getVertex(int i);
         Vertex findVertex(int vert_num);
         std::vector<Vertex> getVertices();
+        vector<vector<string>> vertexToString();
         std::vector<Vertex> getAdjacents(Vertex v);
 
         //label functions for setting label for vertex and edges
@@ -63,14 +64,50 @@ class Graph {
         void setLabel(Vertex& v, std::string label_str);
         std::string getLabel(Edge e);
         void setLabel(Edge& e, std::string label_str);
-
-
         std::string getLabel(Vertex v, Vertex w);
         void setLabel(Vertex& v, Vertex& w, std::string label_str);
- 
+        
+        void convertToAdjacencyList(vector<vector<int>> adjacencyMatrix);
+        vector<vector<int>> getAdjacencyList();
+        vector<vector<int>> getAdjacencyMatrix();
+        void setAdjacencyMatrix(int size);
+        map<pair<string, string>, double> getEdgesToHuesDance();
+        map<pair<string, string>, double> getEdgesToHuesAcc();
+        map<pair<string, string>, double> getEdgesToHuesEnergy();
+
+        void makeEdgeHueMapDance(int size);
+        void makeEdgeHueMapAcc(int size);
+        void makeEdgeHueMapEnergy(int size);
+
+        void changeHue(string song1, string song2, string factor);
+
+        std::vector<string> getAllSongTitles();
+        std::vector<double> getAllSongDance();
+        std::vector<double> getAllSongAcc();
+        std::vector<double> getAllSongEnergy();
+
+        double getDancability(string song);
+        double getAcousticness(string song);
+        double getEnergy(string song);
+
     private:
         std::vector<Vertex> vertices;
+        map<pair<string, string>, double> edges_to_hues_dance;
+        map<pair<string, string>, double> edges_to_hues_acc;
+        map<pair<string, string>, double> edges_to_hues_energy;
+
+        vector<vector<int>> adjacency_matrix;
+        vector<vector<int>> adjacencyList;
+
+        std::vector<string> all_songs;
+        std::vector<double> all_dance;
+        std::vector<double> all_acc;
+        std::vector<double> all_energy;
+
+
         //index represents vertex number
         //could put vector of edges here, but would have to be 2D vector
         
 };
+
+string getSongRecommendation(Graph& g, string& songTitle, const string& category);
